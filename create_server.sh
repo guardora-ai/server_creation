56506865c8df40eb0abfd6266a11079dad58417e
violet <kirsg@yandex.ru>
2024-04-08 13:12:20 +0300

    initial

--- create_server.sh
+++ create_server.sh
@@ -3,15 +3,15 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install linux-headers-$(uname -r)
-sudo apt-get -y install cmake pkg-config mc screen git git-lfs wget unzip python3 python-is-python3 python3-dev python3-pip python3-venv
+sudo NEEDRESTART_MODE=a apt-get -y upgrade
+sudo NEEDRESTART_MODE=a  apt-get -y install linux-headers-$(uname -r)
+sudo NEEDRESTART_MODE=a  apt-get -y install cmake pkg-config mc screen git git-lfs wget unzip python3 python-is-python3 python3-dev python3-pip python3-venv
 
 sudo apt-key del 7fa2af80
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get -y update
-sudo apt -y install cuda-drivers
+sudo  NEEDRESTART_MODE=a apt -y install cuda-drivers
 
 python -m venv ~/guardora
 source ~/guardora/bin/activate
